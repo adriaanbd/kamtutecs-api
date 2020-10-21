c97411ce806c87c225b87d667372f2d25fb6ebdd
Adriaan Beiertz <adriaanbd@gmail.com>
2020-10-20 23:59:28 -0500

    added post route for textract w base64

--- project/app/main.py
+++ project/app/main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from app.config import get_settings, Settings
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/ping')
-async def pong(settings: Settings = Depends(get_settings)):
-    return {
-        "ping": "pong!",
-        "environment": settings.environment,
-        "testing": settings.testing
-    }
+if get_settings().environment == 'dev':
+    origins = [
+        'http://localhost',
+        'http://localhost:3000',
+        'https://locahost:3000',
+    ]
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*'],
+    )
+
+class Image(BaseModel):
+    base64: str
+
+@app.post('/textract')
+async def textract(image: Image):
+    return image
