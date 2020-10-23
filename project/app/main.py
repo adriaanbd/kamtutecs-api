154e27accc7b53ec835e6d1544b49e0e0be3e042
Adriaan Beiertz <adriaanbd@gmail.com>
2020-10-23 00:42:55 -0500

    added bbox for textract with tesseract translate spacy

--- project/app/main.py
+++ project/app/main.py
@@ -8,73 +8,39 @@ import base64
 import numpy as np
 import pytesseract
 import cv2
-from langdetect import detect_langs
-from translate import Translator
+from googletrans import Translator
+import spacy
+
+model_es = spacy.load('es_core_news_sm')
+model_en = spacy.load('en_core_web_sm')
 
 def get_grayscale(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+def get_translate(ocr_str, to_lang):
+    translator = Translator()
 
-def getTranslate(ocr_str, from_lang, to_lang):
-    translator = Translator(from_lang=from_lang, to_lang=to_lang)
-    split_chunks = []
+    clean_str = ''.join(ocr_str.split('\n'))
+    translation = translator.translate(clean_str, dest=to_lang)
 
-    clean_str = ocr_str.split('\n')
+    return translation.text
 
-    for i in range(len(clean_str)):
-        split_chunks.append( translator.translate(clean_str[i]) )
+def detect_lang(img_str):
+    translator = Translator()
+    possible_lang = translator.detect(img_str)
+    return possible_lang.lang
 
-    translation = " ".join(split_chunks)
-
-    return translation
-
-def getLanguages(img_str):
-    possible_lang = detect_langs(img_str)
-
-    return possible_lang
-
-def ocr(img, oem=3,psm=6, lang='en'):
-
-    config = f'--oem {oem} --psm {psm} -l {lang}'
-
-    grey_img = get_grayscale(cv2.imread(img))
-    ocr_text = pytesseract.image_to_string(grey_img, config=config)
-
-    return ocr_text
-
-def ocr_handler(event, context):
-
-    # Extract content from json body
-    body_image64 = event['image64']
-    oem = event["tess-params"]["oem"]
-    psm = event["tess-params"]["psm"]
-    lang = event["tess-params"]["lang"]
-
-    # Decode & save inp image to /tmp
-    with open("/tmp/saved_img.png", "wb") as f:
-      f.write(base64.b64decode(body_image64))
-
-    # Ocr
-    ocr_text = ocr("/tmp/saved_img.png",oem=oem,psm=psm,lang=lang)
-    detect_lang = str(detect_langs(ocr_text)[0])
-    translation = getTranslate(ocr_text, "en", "es")
-
-    # Return the result data in json format
-    payload = {
-      "ocr": ocr_text,
-      "language": detect_lang,
-      "translation": translation
-    }
-
-    return payload
-
+### APP
 app = FastAPI()
+
 settings = get_settings()
 if settings.environment == 'dev':
  origins = [
      'http://localhost',
      'http://localhost:3000',
      'https://locahost:3000',
+     'https://localhost:8000',
+     'http://localhost:8000',
  ]
  app.add_middleware(
      CORSMiddleware,
@@ -84,7 +50,7 @@ if settings.environment == 'dev':
      allow_headers=['*'],
  )
 
-def write_image(image64):
+def write_image_to_png(image64):
     f_name = 'saved_img.png'
     with open(f"./{f_name}", "wb") as f:
         f.write(base64.b64decode(image64))
@@ -101,22 +67,35 @@ class ImageData(BaseModel):
     bbox: BoundingBox
 
 class OCRText(BaseModel):
-    text: str
+    original_text: str
+    translation: str
 
 @app.post('/textract')
-def textract(image_data: ImageData, response_model=OCRText):
+def textract(image_data: ImageData):
     image64 = image_data.base64
     decoded_data = base64.b64decode(image64)
     np_data = np.frombuffer(decoded_data,np.uint8)
     imgBGR = cv2.imdecode(np_data,cv2.IMREAD_UNCHANGED)
-    imgRGB = cv2.cvtColor(imgBGR , cv2.COLOR_BGR2RGB)
-    grey_img = cv2.cvtColor(imgRGB, cv2.COLOR_BGR2GRAY)
-    # grey_img = get_grayscale(cv2.imread(img))
+    x, y, w, h = image_data.bbox.x, image_data.bbox.y, image_data.bbox.w, image_data.bbox.h
+    x, y, w, h = round(float(x)), round(float(y)), round(float(w)), round(float(h))
+    resized_img = cv2.resize(imgBGR, (500, 500))
+    cropped_img = resized_img[y: y + h, x: x + w]
+    grey_img = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2GRAY)
     config = f'--oem 3 --psm 6'
     ocr_text = pytesseract.image_to_string(grey_img, config=config)
-    print(ocr_text)
-    # translation = getTranslate(ocr_text, "en", "es")
-    return ocr_text
+    language = detect_lang(ocr_text)
+    translation = get_translate(ocr_text, "es")
 
-# if '__name__' == '__main__':
-     # uvicorn.run('main:app', host='0.0.0.0', post=8000, debug=True)
+    nlp_es = model_es(translation)
+    nlp_resp = []
+    for word in nlp_es:
+        nlp_resp.append((word.text, word.pos_))
+
+    return {
+        "ocr_text": ocr_text,
+        "input_text": language,
+        "translation": translation,
+        "bbox": image_data.bbox,
+        "nlp": nlp_resp
+    }
+
